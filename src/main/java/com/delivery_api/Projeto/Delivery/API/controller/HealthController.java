078677dc73757fdc78ca7e8c4aa8d85b4e1b94c0
Álvaro Lomeu <alvarolomeu@gmail.com>
2025-11-02 05:08:53 -0300

    Health Controller

---
+++ src/main/java/com/delivery_api/Projeto/Delivery/API/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.delivery_api.Projeto.Delivery.API.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestController // Spring, essa classe vai lidar com requisições HTTP
+public class HealthController {
+
+    @GetMapping("/health") // Spring, quando alguém acessar /health com GET, chame esse metodo
+    public Map<String, String> health(){
+        return Map.of(
+            "status", "UP",
+            "timestamp", LocalDateTime.now().toString(),
+            "Service", "Delivery API"
+        );
+    }
+}
